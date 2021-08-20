99b8ac5605b04fa1d390ecca4022e8099f4063f8
SmaHonda <86571238+SmaHonda@users.noreply.github.com>
2021-08-20 21:01:38 +0800

    0820

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    title: 'PROJECT',
+    description: '',
+    base: '/MFEE18_project/',
+    repo: 'https://github.com/SmaHonda/MFEE18_project',
+    themeConfig: {
+      sidebarDepth: 0,
+      sidebar: [
+        ['/', '前言'],
+        {
+            title: '主要元件',
+            collapsable: false,
+            children: [
+              '/product/add',
+            //   '/component/state-storage-and-locking',
+            //   '/component/backends',
+            ]
+          },
+      ],
+      nav: [
+        { text: 'Home', link: '/' },
+        // { text: 'Github', link: 'https://github.com/SmaHonda/MFEE18_project' }
+      ]
+    }
+  }
